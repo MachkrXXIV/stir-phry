00b858adc0468f7a8397ae60af907244b10d9ade
Justin Mach <bachmach24k@gmail.com>
2023-05-18 15:21:25 -0500

    add: routing to home

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbCarousel } from '@ng-bootstrap/ng-bootstrap';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
