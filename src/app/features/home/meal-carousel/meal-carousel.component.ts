42b30e7bd4138ed5d24e68b605778dce981691fe
Justin Mach <bachmach24k@gmail.com>
2023-05-18 23:17:14 -0500

    add: mobile meal carousel

--- src/app/features/home/meal-carousel/meal-carousel.component.ts
+++ src/app/features/home/meal-carousel/meal-carousel.component.ts
@@ -1,18 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { Meal } from 'src/app/shared/interfaces/meal.interface';
-import { MealCarouselComponent } from './meal-carousel/meal-carousel.component';
+import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss'],
+  selector: 'app-meal-carousel',
+  templateUrl: './meal-carousel.component.html',
+  styleUrls: ['./meal-carousel.component.scss'],
+  standalone: true,
+  imports: [NgbCarouselModule, CommonModule],
 })
-export class HomeComponent implements OnInit {
+export class MealCarouselComponent implements OnInit {
   constructor() {}
-
-  isLargeScreen = false;
-
-  userName = 'Justin';
 
   meals: Meal[] = [
     {
