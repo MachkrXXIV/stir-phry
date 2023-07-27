66799b0afe56de60f9a7e497f2cb0b3783947997
Justin Mach <bachmach24k@gmail.com>
2023-07-27 17:55:08 -0500

    refactor: card components extend from baseCard

--- src/app/shared/components/query-card/query-card.component.ts
+++ src/app/shared/components/query-card/query-card.component.ts
@@ -3,31 +3,35 @@ import { Meal } from '../../interfaces/meal.interface';
 import { RecipeService } from '../../services/recipe.service';
 import { Router } from '@angular/router';
 import { FirestoreRecipesService } from '../../services/firestore-recipes.service';
+import { BaseCard } from '../../interfaces/base-card.interface';
 @Component({
   selector: 'app-query-card',
   templateUrl: './query-card.component.html',
   styleUrls: ['./query-card.component.scss'],
 })
-export class QueryCardComponent implements OnInit {
+export class QueryCardComponent extends BaseCard implements OnInit {
   @Input() query: Meal = {
     id: '',
     name: '',
     image: '',
   };
   isUserCreated!: boolean;
+  isSaved?: boolean;
 
   constructor(
-    private recipeService: RecipeService,
-    private router: Router,
-    private savedMealService: FirestoreRecipesService
-  ) {}
+    router: Router,
+    firestoreRecipeService: FirestoreRecipesService,
+    private recipeService: RecipeService
+  ) {
+    super(router, firestoreRecipeService);
+  }
 
-  routeToDetailedView(query: Meal, isUserCreated: boolean) {
+  override routeToDetailedView(id: string) {
     const path = '/saved-meals';
-    if (isUserCreated) {
-      this.savedMealService.get(query.id, path).subscribe({
+    if (this.isUserCreated || this.isSaved) {
+      this.firestoreRecipeService.get(id, path).subscribe({
         next: (recipe: Meal) => {
-          this.router.navigate(['/collections/saved-meals', query.id], {
+          this.router.navigate(['/collections/saved-meals', id], {
             state: recipe,
           });
         },
@@ -37,7 +41,7 @@ export class QueryCardComponent implements OnInit {
       });
     } else {
       console.log('not user');
-      this.recipeService.getDetailedInformation(query.id).subscribe({
+      this.recipeService.getDetailedInformation(id).subscribe({
         next: (recipe: Meal) => {
           let nameUrl = recipe.name.split(' ').join('-');
           console.log(recipe);
@@ -56,5 +60,6 @@ export class QueryCardComponent implements OnInit {
     this.isUserCreated = this.query.id.toString().startsWith('CUSTOM')
       ? true
       : false;
+    this.isSaved = localStorage.getItem(this.query.id) ? true : false;
   }
 }
