c4bf51ec907c7c3e51dd5af5061af42db80bb4f8
Justin Mach <bachmach24k@gmail.com>
2023-07-30 16:13:41 -0500

    add: skeletal cook component

---
+++ src/app/features/cook/cook.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CookComponent } from './cook.component';
+
+@NgModule({
+  declarations: [CookComponent],
+  imports: [CommonModule],
+})
+export class CookModule {}
