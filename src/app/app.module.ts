0b2c12e5eebc3f210a4096ea1d842f109b0505a3
Justin Mach <bachmach24k@gmail.com>
2023-08-05 18:54:37 -0500

    refactor: heading color and navbar size

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,6 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AngularFireModule } from '@angular/fire/compat';
-import { HeaderComponent } from './shared/components/header/header.component';
 import {
   FirestoreModule,
   provideFirestore,
@@ -20,20 +19,8 @@ import {
   FaIconLibrary,
   FontAwesomeModule,
 } from '@fortawesome/angular-fontawesome';
-import { HomeComponent } from './features/home/home.component';
-import { DiscoverComponent } from './features/discover/discover.component';
-import { SearchBarComponent } from './shared/components/search-bar/search-bar.component';
-import { CreateRecipeComponent } from './shared/components/create-recipe/create-recipe.component';
-import { ExplorerComponent } from './features/discover/explorer/explorer.component';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { far } from '@fortawesome/free-regular-svg-icons';
-import { BannerCardComponent } from './shared/components/banner-card/banner-card.component';
-import { QueryCardComponent } from './shared/components/query-card/query-card.component';
-import { DetailedViewComponent } from './features/discover/detailed-view/detailed-view.component';
-import { CamelCaseSplitPipe } from './shared/camel-case-split.pipe';
-import { NavTabsComponent } from './features/discover/detailed-view/nav-tabs/nav-tabs.component';
-import { SliderContainerComponent } from './shared/components/slider-container/slider-container.component';
-import { RecipePreviewComponent } from './features/home/recipe-preview/recipe-preview.component';
 import { CommonModule } from '@angular/common';
 import { HomeModule } from './features/home/home.module';
 import { CookModule } from './features/cook/cook.module';
