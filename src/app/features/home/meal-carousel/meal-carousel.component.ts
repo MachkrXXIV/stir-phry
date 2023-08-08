173ebccbc4ecc67a4758830fede787e6876aee91
Justin Mach <bachmach24k@gmail.com>
2023-08-08 14:43:12 -0500

    add: remove button for agenda meals

--- src/app/features/home/meal-carousel/meal-carousel.component.ts
+++ src/app/features/home/meal-carousel/meal-carousel.component.ts
@@ -4,6 +4,7 @@ import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
 import { CommonModule } from '@angular/common';
 import { Observable } from 'rxjs';
 import { FirestoreRecipesService } from 'src/app/shared/services/firestore-recipes.service';
+import { RecipeStateMachineService } from 'src/app/core/services/recipe-state-machine.service';
 
 @Component({
   selector: 'app-meal-carousel',
@@ -13,7 +14,19 @@ import { FirestoreRecipesService } from 'src/app/shared/services/firestore-recip
 export class MealCarouselComponent implements OnInit {
   mealAgenda$?: Observable<Meal[]>;
   isLargeScreen = false;
-  constructor(private firestore: FirestoreRecipesService) {}
+  constructor(
+    private firestore: FirestoreRecipesService,
+    private stateMachine: RecipeStateMachineService
+  ) {}
+
+  removeAgendaMeal(agendaMeal: Meal) {
+    const key = agendaMeal.id;
+    const state = this.stateMachine.getRecipeState(key);
+    state.isTried = false;
+    this.firestore.delete(agendaMeal, '/meal-agenda');
+    this.stateMachine.setRecipeState(key, state);
+    this.stateMachine.deleteEmpty(key, state);
+  }
 
   ngOnInit(): void {
     const path = '/meal-agenda';
