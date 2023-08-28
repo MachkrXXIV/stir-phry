109f16e98f11e38af38c3dc8727a530fd16a46b0
Justin Mach <bachmach24k@gmail.com>
2023-08-27 22:33:40 -0500

    build: increase budget limits

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -9,5 +9,19 @@ export const environment = {
     messagingSenderId: '1080804122304',
     measurementId: 'G-HRZ6ZKWXPP',
   },
-  production: true
+  production: true,
+
+  apiKey: 'f0327ba727ab405e9b9c75c25ee38e8e',
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  firebaseConfig: {
+    apiKey: 'AIzaSyAdxxQ79x7COZ5XjZkyJ8gcktcYBbFlmcE',
+    authDomain: 'stir-phry.firebaseapp.com',
+    projectId: 'stir-phry',
+    storageBucket: 'stir-phry.appspot.com',
+    messagingSenderId: '1080804122304',
+    appId: '1:1080804122304:web:1ce189263db1f8ffbbd725',
+    measurementId: 'G-HRZ6ZKWXPP',
+  },
 };
